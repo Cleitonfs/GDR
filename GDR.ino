9e3b1a28ef576d8ff7773303d59684f0ec33373f
Genilton Cleiton <cleitonfs@yahoo.com>
2017-12-10 13:43:11 -0300

    Update comment correction

--- GDR.ino
+++ GDR.ino
@@ -2,7 +2,7 @@
 ------------------------------------------GDR - Guide Dog Robot---------------------------------------
 Versão do Software: 1.0
 ******************************************************************************************************/
-Update: 10-12-17 @genilton.cleiton@ufrpe.br
+//Update: 10-12-17 @genilton.cleiton@ufrpe.br
   
   // inclusão de bibliotecas.    
   #include <Servo.h>    // inclui biblioteca de manipulação do servo motor.    
